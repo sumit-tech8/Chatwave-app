f00cbda3f1be082e0f0d07b546d5fc81a198cb7b
sumit-tech8 <134231779+sumit-tech8@users.noreply.github.com>
2024-02-03 20:01:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -19,11 +19,7 @@ connectDB();
 const app = express();
 
 //middelwares
-app.use(cors(
-  {
-    origin: ["https://deploy-chat-app.vercel.app"
-  }
-));
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 // app.use(express.static(path.join(__dirname, './my-app/build')))
